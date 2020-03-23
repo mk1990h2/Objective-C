b93a745b1715a91a5a7f6f47a5f478038ffc74c4
mk1990h2 <mikass111@yahoo.co.jp>
2020-03-23 15:44:24 -0700

    add lab1

---
+++ Lab1/Lab1/main.m
@@ -0,0 +1,69 @@
+//
+//  main.m
+//  Lab1
+//
+//  Created by 佐藤美佳 on 2020/03/23.
+//  Copyright © 2020 Mika Sato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString *getUserInput(int maxLength, NSString *prompt) {
+    if (maxLength < 1) {
+        maxLength = 255;
+    }
+    NSLog(@"%@ ", prompt);
+    char inputChars[maxLength];
+    char *result = fgets(inputChars, maxLength, stdin);
+    if (result != NULL) {
+        // turn CString to NSString
+        // NSString.stringWithUTF8String(inputChars)
+        // .stringByTrimmingCharactersInSet(
+        //      NSCharacterSet.whitespaceCharacterSet);
+        return [[NSString stringWithUTF8String: inputChars] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    }
+    return NULL;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        while (YES) {
+            NSString *strInput = getUserInput(255, @"¥nEnter your String: ('q' to quit");
+            if ([strInput isEqualToString:@"q"]) {
+                break;
+            }
+            while (YES) {
+                NSString *option = getUserInput(10, @"\nChoose one of the following options\n1. Uppercase\n2. Lowercase\n3. Numberize\n4. Canadianize\n5. Resond\n6. De-Space-It\n7. Done\n");
+                if ([option isEqualToString:@"7"]) {
+                    break;
+                } else if ([option isEqualToString:@"1"]) {
+                    NSLog(@"%@", [strInput uppercaseString]);
+                } else if ([option isEqualToString:@"2"]) {
+                    NSLog(@"%@", [strInput lowercaseString]);
+                } else if ([option isEqualToString:@"3"]) {
+                    // check if strInput is number
+                    // NSNumberFormatter fomatter = new NSNumberFormatter();
+                    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+                    NSNumber *number = [formatter numberFromString: strInput];
+                    if (number != nil) {
+                        // integerValue is Long value
+                        NSLog(@"%ld", [number integerValue]);
+                    } else {
+                        NSLog(@"%@", @"Input cannot be converted to Integer");
+                    }
+                } else if ([option isEqualToString:@"4"]) {
+                    NSLog(@"%@", [strInput stringByAppendingString:@", eh?"]);
+                } else if ([option isEqualToString:@"5"]) {
+                    if ([strInput characterAtIndex:[strInput length] - 1] == '?') {
+                        NSLog(@"%@", @"I don't know");
+                    } else if ([strInput characterAtIndex:[strInput length] - 1] == '!') {
+                        NSLog(@"%@", @"Whoa, calm down!");
+                    }
+                } else if ([option isEqualToString:@"6"]) {
+                    NSLog(@"%@", [strInput stringByReplacingOccurrencesOfString:@" " withString:@"-"]);
+                }
+            }
+        }
+    }
+    return 0;
+}
